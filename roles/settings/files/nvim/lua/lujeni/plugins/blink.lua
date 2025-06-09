5a24a8e3275e9bd9294c104a8451a755c828ec7d
Lujeni <julien@thebault.co>
2025-06-09 23:02:42 +0200

    feat(neovim): pump blink

--- roles/settings/files/nvim/lua/lujeni/plugins/blink.lua
+++ roles/settings/files/nvim/lua/lujeni/plugins/blink.lua
@@ -2,15 +2,34 @@ return {
 	"saghen/blink.cmp",
 	version = "1.*",
 	opts = {
-		keymap = { preset = "default" },
-
-		appearance = {
-			-- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-			-- Adjusts spacing to ensure icons are aligned
-			nerd_font_variant = "mono",
+		keymap = {
+			preset = "enter",
+			["<C-space>"] = { "select_and_accept" },
 		},
-
-		completion = { documentation = { auto_show = false } },
+		appearance = {
+			nerd_font_variant = "mono",
+			use_nvim_cmp_as_default = true,
+		},
+		signature = {
+			enabled = true,
+		},
+		completion = {
+			accept = {
+				auto_brackets = {
+					enabled = true,
+				},
+			},
+			menu = {
+				draw = {
+					treesitter = { "lsp" },
+					columns = { { "label", gap = 1 }, { "kind_icon", gap = 1, "kind" } },
+				},
+			},
+			documentation = {
+				auto_show = true,
+				auto_show_delay_ms = 200,
+			},
+		},
 		sources = {
 			default = { "lazydev", "lsp", "path", "snippets", "buffer" },
 			providers = {
@@ -22,7 +41,6 @@ return {
 				},
 			},
 		},
-
 		fuzzy = { implementation = "prefer_rust_with_warning" },
 	},
 	opts_extend = { "sources.default" },
